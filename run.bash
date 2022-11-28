96282e43244ff8a3c3bc7f6d5069184d73820fa6
William <william.wu.xiaoxiang@gmail.com>
2022-11-28 17:42:01 +1100

    m

--- run.bash
+++ run.bash
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
 
+#num_processor=$(grep -c ^processor /proc/cpuinfo)
+num_processor=7
+width=1024
+gcc main.c -fopenmp || exit
+
+echo "" >>output.txt
+echo "$width x $width" >>output.txt
+
+for i in $(seq 1 "$num_processor"); do
+  echo "Welcome $i x3 times"
+  printf '%d ' "$i" >>output.txt
+
+  for j in {1..3}; do
+    ./a.out "$width" "$i" 2>>output.txt
+  done
+
+  echo "" >>output.txt
+
+done
